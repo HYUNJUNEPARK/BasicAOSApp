5f3ef97da0ea6809fc7b0f2964a3dacaede07720
HYUNJUNEPARK <june2ac4dev@gmail.com>
2021-12-31 21:52:11 +0900

    function2

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.randomnumbergenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
@@ -12,7 +11,7 @@ import com.example.randomnumbergenerator.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private var checker: Boolean = false //createButton 버튼이 이미 눌렸는지 확인
-    private val pickNumberSet = hashSetOf<Int>() //chooseNumberButton 버튼으로 숫자 생성하고 createButton 버튼을 눌렀을 때 중복을 피함(mutableSetOf<Int>()를 사용해도 됨)
+    private val pickedNumberSet = hashSetOf<Int>() //chooseNumberButton 버튼으로 숫자 생성하고 createButton 버튼을 눌렀을 때 중복을 피함(mutableSetOf<Int>()를 사용해도 됨)
     private val listOfChosenNumberTextView: List<TextView> by lazy {
         listOf<TextView>(
             binding.chosenNumberOne,
@@ -34,39 +33,44 @@ class MainActivity : AppCompatActivity() {
         //[END NumberPicker 번호 세팅]
 
         binding.createButton.setOnClickListener {
-            val numbers:List<Int> = makeRandomNumber()
-
+            val numberList:List<Int> = makeRandomNumber()
+            checker = true
+            numberList.forEachIndexed { index, number ->
+                val chosenNumberTextView = listOfChosenNumberTextView[index]
+                chosenNumberTextView.isVisible = true
+                chosenNumberTextView.text = number.toString()
+            }
         }
-
+        binding.clearButton.setOnClickListener {
+            checker = false
+            pickedNumberSet.clear()
+            listOfChosenNumberTextView.forEach {
+                it.isVisible = false
+            }
+        }
         binding.chooseNumberButton.setOnClickListener {
             //[START 예외처리]
             if(checker){ //이미 createButton 버튼을 눌러 6개 번호를 얻었다면 토스트 메세지를 띄움
                 Toast.makeText(this, "초기화 후 시도해주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if (pickNumberSet.size >= 5) {
+            if (pickedNumberSet.size >= 5) {
                 Toast.makeText(this, "지정 번호는 최대 5개까지 가능합니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if (pickNumberSet.contains(binding.numberPicker.value)){
+            if (pickedNumberSet.contains(binding.numberPicker.value)){
                 Toast.makeText(this, "이미 지정된 번호입니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             //[END 예외처리]
 
-            val chosenNumberTextView = listOfChosenNumberTextView[pickNumberSet.size]
+            //[START NumberPicker 값 UI 세팅]
+            val chosenNumberTextView = listOfChosenNumberTextView[pickedNumberSet.size]
             chosenNumberTextView.isVisible = true
             chosenNumberTextView.text = binding.numberPicker.value.toString()
-            pickNumberSet.add(binding.numberPicker.value)
-
+            pickedNumberSet.add(binding.numberPicker.value)
+            //[END NumberPicker 값 UI 세팅]
         }
-
-        binding.clearButton.setOnClickListener {
-            Log.d("testLog", "33333333333333")
-        }
-
-
-
     }
 
     private fun makeRandomNumber(): List<Int> {
@@ -80,13 +84,17 @@ class MainActivity : AppCompatActivity() {
         //[END 방법1. set 을 이용해 랜덤 숫자 생성]
 
         //[START 방법2. list 를 이용해 램덤 숫자 생성]
-        val _numberList = mutableListOf<Int>().apply{
-            for(i in 1..45) {
-                this.add(i)
+        val _numberList = mutableListOf<Int>()
+            .apply {
+                for (i in 1..45) {
+                    if(pickedNumberSet.contains(i)) {
+                        continue
+                    }
+                    this.add(i)
+                }
             }
-        }
         _numberList.shuffle()
-        val numberList = _numberList.subList(0, 6)
+        val numberList = pickedNumberSet.toList() + _numberList.subList(0, 6 - pickedNumberSet.size)
         return numberList.sorted()
         //[END 방법2. list 를 이용해 램덤 숫자 생성]
     }
