2a4b3c56f6904224647a063b20c4e7e274f42301
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-01-05 23:46:52 +0900

    LogInAndPasswordChange

--- app/src/main/java/com/june/secretdiary/MainActivity.kt
+++ app/src/main/java/com/june/secretdiary/MainActivity.kt
@@ -1,11 +1,104 @@
 package com.june.secretdiary
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.edit
+import com.june.secretdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var changePasswordMode = false //비밀번호를 수정하는 동안 다른 작업을 할 수 없게 하는 변수
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        //[START Setting numberPicker]
+        binding.numberPicker1.apply {
+            minValue = 0
+            maxValue = 9
+        }
+        binding.numberPicker2.apply {
+            minValue = 0
+            maxValue = 9
+        }
+        binding.numberPicker3.apply {
+            minValue = 0
+            maxValue = 9
+        }
+        //[END Setting numberPicker]
+
+        //SharedPreferences 생성
+        val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+        binding.openButton.setOnClickListener {
+            //[START 예외처리]
+            if (changePasswordMode) {
+                Toast.makeText(this, "비밀번호 변경 중입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            //[END 예외처리]
+
+            //[START 비밀번호 확인 -> 페이지 이동 or AlertDialog]
+            val passwordFromUser = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
+            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {//password 이름을 갖는 SharedPreferences 에 저장된 String 값을 꺼내와 passwordFromUser 값과 비교 (SharedPreferences 에 저장된 값이 없다면 디폴트 값은 000)
+                //startActivity()
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+            } else {
+                showAlertDialog()
+            }
+            //[END 비밀번호 확인 -> 페이지 이동 or AlertDialog]
+        }
+
+        binding.changePasswordButton.setOnClickListener {
+            val passwordFromUser = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
+
+            //changePasswordMode 활성화(true) -> changePasswordMode 비활성화(false)
+            //비밀번호 변경 가능
+            if (changePasswordMode) {
+                /* SharedPreferences 값 수정
+                - edit 함수에 람다로 SharedPreferences 값 수정 가능
+                - commit() : 동기적 저장 (파일이 다 저장될때까지 UI를 멈추고 기다림)
+                - apply() : 비동기적 저장 */
+                passwordPreferences.edit {
+                    putString("password", passwordFromUser)
+                    commit()
+                }
+                changePasswordMode = false
+                binding.changePasswordButton.setBackgroundColor(Color.BLACK)
+                initializeNumberPicker()
+                Toast.makeText(this, "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show()
+
+            //changePasswordMode 비활성화(false) -> changePasswordMode 활성화(true)
+            } else {
+                if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                    binding.changePasswordButton.setBackgroundColor(Color.RED)
+                } else {
+                    showAlertDialog()
+                }
+            }
+        }
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Page Open Error")
+            .setMessage("비밀번호가 잘못되었습니다.")
+            .setPositiveButton("확인") { _, _ ->
+                initializeNumberPicker()
+            }
+            .create()
+            .show()
+    }
+    private fun initializeNumberPicker(){
+        binding.numberPicker1.value = 0
+        binding.numberPicker2.value = 0
+        binding.numberPicker3.value = 0
     }
 }
