39e57222bc0f35e84d52a7385d0d80ba25b668cb
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-01-09 21:51:12 +0900

    SaveDiaryActivity

--- app/src/main/java/com/june/secretdiary/DiaryActivity.kt
+++ app/src/main/java/com/june/secretdiary/DiaryActivity.kt
@@ -1,16 +1,38 @@
 package com.june.secretdiary
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
 import com.june.secretdiary.databinding.ActivityDiaryBinding
 
 class DiaryActivity : AppCompatActivity() {
     private val binding by lazy { ActivityDiaryBinding.inflate(layoutInflater) }
+    private val handler = Handler(Looper.getMainLooper()) //메인 스레드에 연결된 핸들러
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //SharedPreferences 생성 & Preference 저장된 값 가져오기
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+        binding.diraryEditText.setText(detailPreferences.getString("detail", ""))
 
+        val runnable = Runnable {
+            //비동기적 저장
+            getSharedPreferences("diary", Context.MODE_PRIVATE).edit(false) {
+                putString("detail", binding.diraryEditText.text.toString())
+            }
+        }
+
+        //텍스트 수정을 잠시 멈추면(500ms) 자동 저장
+        binding.diraryEditText.addTextChangedListener {
+            handler.removeCallbacks(runnable) //postDelayed() 가 실행되기 전에 runnable 이 있다면 지워줌
+            handler.postDelayed(runnable, 500) //500ms 에 한번씩 runnable 호출
+        }
     }
 }
--- app/src/main/java/com/june/secretdiary/MainActivity.kt
+++ app/src/main/java/com/june/secretdiary/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.june.secretdiary
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -45,11 +46,10 @@ class MainActivity : AppCompatActivity() {
 
             //[START 비밀번호 확인 -> 페이지 이동 or AlertDialog]
             val passwordFromUser = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
-
             /* SharedPreferences 에 저장된 값 꺼내오기
             SharedPreferences 객체.get데이터타입("SharedPreferences 이름", "default value") */
             if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
-                //startActivity()
+                startActivity(Intent(this, DiaryActivity::class.java))
             } else {
                 showAlertDialog()
             }
@@ -58,22 +58,19 @@ class MainActivity : AppCompatActivity() {
 
         binding.changePasswordButton.setOnClickListener {
             val passwordFromUser = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
-
             //changePasswordMode 활성화(true) -> 비밀번호 변경 가능 -> changePasswordMode 비활성화(false)
             if (changePasswordMode) {
                 /* SharedPreferences 값 수정
                 - edit 함수에 람다로 SharedPreferences 값 수정 가능
-                - commit() : 동기적 저장 (파일이 다 저장될때까지 UI를 멈추고 기다림)
+                - commit() : 동기적 저장 (파일이 다 저장될때까지 UI를 멈추고 기다림) - 가벼운 작업에서 사용
                 - apply() : 비동기적 저장 */
-                passwordPreferences.edit {
+                passwordPreferences.edit(true) {
                     putString("password", passwordFromUser)
-                    commit()
                 }
                 changePasswordMode = false
                 binding.changePasswordButton.setBackgroundColor(Color.BLACK)
                 initializeNumberPicker()
                 Toast.makeText(this, "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show()
-
             //changePasswordMode 비활성화(false) -> changePasswordMode 활성화(true)
             } else {
                 if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
@@ -86,7 +83,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     private fun showAlertDialog() {
         AlertDialog.Builder(this)
             .setTitle("Page Open Error")
