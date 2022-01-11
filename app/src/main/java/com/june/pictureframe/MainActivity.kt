3d571a5e50f360caba5e87d732c10ad4f2abe638
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-01-12 00:32:27 +0900

    takeImage

--- app/src/main/java/com/june/pictureframe/MainActivity.kt
+++ app/src/main/java/com/june/pictureframe/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.june.pictureframe
 
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.june.pictureframe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(binding.imageView1)
+            add(binding.imageView2)
+            add(binding.imageView3)
+            add(binding.imageView4)
+            add(binding.imageView5)
+            add(binding.imageView6)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +41,47 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     //권한 부여 되었을 때 갤러리에서 사진 선택
+                    navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)-> {
+                    //권한이 없다면 팝업 메세지
                     showPermissionContextPopup()
                 }
                 else ->{
+                    //권한 요청
                     requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
                 }
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //권한 부여 -> 사진 가져옴
+                        navigatePhotos()
+                } else {
+                  Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                //XXX 구현 안해도 됨
+            }
+        }
+    }
+
+    private fun navigatePhotos() {
+        //SAF 기능 이용
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
     }
 
     private fun showPermissionContextPopup() {
