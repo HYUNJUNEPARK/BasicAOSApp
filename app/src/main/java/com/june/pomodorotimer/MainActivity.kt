784390c912333884983604a94ebedd7976ea6c52
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-01-24 23:47:05 +0900

    Timer

--- app/src/main/java/com/june/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/june/pomodorotimer/MainActivity.kt
@@ -2,11 +2,14 @@ package com.june.pomodorotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import android.widget.SeekBar
 import com.june.pomodorotimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var currentCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +27,52 @@ class MainActivity : AppCompatActivity() {
                     fromUser : 유저가 발생시킨 이벤트 일 때
                     progress : 유저 상관 없이 변화 생겼을 때
                     */
-                    binding.remainMinutesTextView.text = "%02d".format(progress)
+                    if (fromUser) {
+                        val initialMills = progress * 60 * 1000L
+                        updateRemainTime(initialMills)
+                    }
+                }
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    currentCountDownTimer?.cancel() //이미 진행중인 타이머가 있을 때 cancel
+                    currentCountDownTimer = null
 
                 }
-                override fun onStartTrackingTouch(p0: SeekBar?) {
-                }
-                override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {//시크바에 손을 떼었을 때
+                    if (seekBar == null) {
+                        return@onStopTrackingTouch
+                    } else {
+                        val seekBarMinutes: Int = seekBar.progress
+                        val initialMills: Long = seekBarMinutes * 60 * 1000L
+                        currentCountDownTimer = createCountDownTimer(initialMills)
+                        currentCountDownTimer?.start()
+                    }
                 }
             }
         )
     }
+    //See: https://developer.android.com/reference/android/os/CountDownTimer
+    private fun createCountDownTimer(initialMills: Long): CountDownTimer {
+        return object : CountDownTimer(initialMills, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {//1초마다 한번씩 호출됨
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+            override fun onFinish() {
+                updateRemainTime(0)
+                updateSeekBar(0)
+            }
+        }
+    }
 
+    private fun updateRemainTime(remainMills: Long) {
+        val remainSeconds = remainMills / 1000
+        binding.remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        binding.remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
 
+    private fun updateSeekBar(remainMills: Long){
+        val remainMinutes: Int = (remainMills / 1000 / 60).toInt()
+        binding.seekBar.progress =  remainMinutes
+    }
 }
