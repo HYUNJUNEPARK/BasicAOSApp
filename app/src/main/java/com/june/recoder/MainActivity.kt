b302b669c476bc03424f190df5148a0aea128792
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-02-07 23:08:08 +0900

    UI

--- app/src/main/java/com/june/recoder/MainActivity.kt
+++ app/src/main/java/com/june/recoder/MainActivity.kt
@@ -6,11 +6,16 @@ import com.june.recoder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        initViews()
+    }
 
+    private fun initViews() {
+        binding.recordButton.updateIconWithState(state)
     }
 }
