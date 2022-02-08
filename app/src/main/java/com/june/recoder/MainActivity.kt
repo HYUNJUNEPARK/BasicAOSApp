9f0aea48d1b6812aab2bb9a82b6990020b7a7068
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-02-08 22:09:14 +0900

    auth

--- app/src/main/java/com/june/recoder/MainActivity.kt
+++ app/src/main/java/com/june/recoder/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.june.recoder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.june.recoder.databinding.ActivityMainBinding
@@ -7,15 +9,38 @@ import com.june.recoder.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private var state = State.BEFORE_RECORDING
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         initViews()
+        requestAudioPermission()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted: Boolean =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                    grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (!audioRecordPermissionGranted) {
+            finish()
+        }
     }
 
     private fun initViews() {
         binding.recordButton.updateIconWithState(state)
     }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
 }
