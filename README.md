54708d8cf4d3d625b1b7e92ae8a0aa17b9e30ed7
박현준(Hyunjune Park) <parkhj@konai.com>
2022-02-10 16:44:51 +0900

    abstractOnCreate

--- README.md
+++ README.md
@@ -1,31 +1,55 @@
 # RandomNumberGenerator
 
-![1_Screenshot_20211231-231107_RandomNumberGenerator](https://user-images.githubusercontent.com/89306567/147827749-dab3791e-7b92-4c26-9e67-265ea4cd1e65.jpg) <br />
-랜덤 번호 6 개를 생성해주는 앱 <br />
-번호 지정 버튼으로 6가지 랜덤 번호 중 최대 5가지 번호를 지정할 수 있으며 번호 생성 버튼으로 랜덤 번호를 생성 
+<img src="https://github.com/HYUNJUNEPARK/ImageRepository/blob/master/2_RandomNumberGenerator.jpg" height="400"/>
 
-#새로 배운 기능
-1. contraintLayout 속성<br />
-<Button <br />
-... <br />
-   app:layout_constraintHorizontal_chainStyle="packed" <br />
-... <br />
-/> <br />
+---
+1. <a href = "#content1">Shape Drawable</a></br>
+2. <a href = "#content2"> NumberPicker</a></br>
+3. <a href = "#content3">forEachIndexed</a></br>
+* <a href = "#ref">참고링크</a>
+---
+><a id = "content1">**1. Shape Drawable**</a></br>
 
-2. shape drawable<br />
-- drawable 우클릭 new -> Drawable Resource File<br />
-- XML 파일로 쉽게 이미지를 생성할 수 있음<br />
-- 안드로이드 앱 내부에 있는 XML 파일로 불러오려면 context가 필요함<br />
-textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)<br />
+XML 로 쉽게 Drawable 객체를 생성하며 APK 용량을 줄여주고 쉽게 모양을 바꿀 수 있음
 
-3. NumberPicker<br />
-- minValue, maxValue 로 범위 지정<br />
-binding.numberPicker.minValue = 1<br />
-binding.numberPicker.maxValue = 45<br />
+배경이미지를 만들 때 사용
 
-4. forEachIndexed<br />
-- 객체뿐 아니라 인덱스 데이터도 필요할 때 사용<br />
- numberList.forEachIndexed { index, number -> <br />
-   ..<br />
-}<br />
+drawable 우클릭 new -> Drawable Resource File -> Root Element 에 shape 입력
 
+```xml
+<shape android:shape="[rectangle|oval|line|ring]"
+    xmlns:android="http://schemas.android.com/apk/res/android">
+    <solid android:color="@color/orange"/>
+    <size
+        android:width="15dp"
+        android:height="15dp"/>
+</shape>
+```
+
+Shape Drawable 을 불러오려면 context 가 필요
+
+`textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)`
+
+><a href = "#content2">**2. NumberPicker**</a></br>
+
+minValue, maxValue 로 범위 지정해 사용
+
+```kotlin
+binding.numberPicker.minValue = 1
+binding.numberPicker.maxValue = 45
+```
+
+><a href = "#content3">**3. forEachIndexed**</a></br>
+
+객체뿐 아니라 인덱스 데이터도 필요할 때 사용
+
+```kotlin
+numberList.forEachIndexed { index, number ->
+//...    
+}
+```
+---
+><a id = "ref">**참고링크**</a></br>
+
+Shape Drawable</br>
+https://overoid.tistory.com/30</br>
--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
@@ -11,8 +11,8 @@ import com.example.randomnumbergenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private var checker: Boolean = false //createButton 버튼이 이미 눌렸는지 확인
-    private val pickedNumberSet = hashSetOf<Int>() //chooseNumberButton 버튼으로 숫자 생성하고 createButton 버튼을 눌렀을 때 중복을 피함(mutableSetOf<Int>()를 사용해도 됨)
+    private var checkerPressedCNB: Boolean = false //CNB(CreateNumberButton)
+    private val pickedNumberSet = hashSetOf<Int>() //choiceNumberButton 버튼으로 숫자 생성하고 createButton 버튼을 눌렀을 때 중복을 피함(mutableSetOf<Int>()를 사용해도 됨)
     private val listOfChosenNumberTextView: List<TextView> by lazy {
         listOf<TextView>(
             binding.chosenNumberOne,
@@ -28,14 +28,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //[START NumberPicker 번호 세팅]
+        initNumberPicker()
+        initCreateNumberButton()
+        initChoiceNumberButton()
+        initClearButton()
+    }
+
+    private fun initNumberPicker() {
         binding.numberPicker.minValue = 1
         binding.numberPicker.maxValue = 45
-        //[END NumberPicker 번호 세팅]
+    }
 
-        binding.createButton.setOnClickListener {
+    private fun initCreateNumberButton() {
+        binding.createNumberButton.setOnClickListener {
             val numberList:List<Int> = makeRandomNumber()
-            checker = true
+            checkerPressedCNB = true
+
             numberList.forEachIndexed { index, number ->
                 val chosenNumberTextView = listOfChosenNumberTextView[index]
                 chosenNumberTextView.isVisible = true
@@ -43,25 +51,59 @@ class MainActivity : AppCompatActivity() {
                 chosenNumberTextView.text = number.toString()
             }
         }
+    }
+
+    private fun makeRandomNumber(): List<Int> {
+        //방법1. set 을 이용해 랜덤 숫자 생성
+//        val numberSet = mutableSetOf<Int>()
+//        while(numberSet.size < 6) {
+//            val rangeNumber = (1..45).random()
+//            numberSet.add(rangeNumber)
+//        }
+//        return numberSet
+
+        //방법2. list 를 이용해 램덤 숫자 생성
+        //사용자가 미리 뽑은 값에 따라 최소 39개(6개 숫자 선택)에서 최대 45개(선택 값 없음) 랜덤한 수를 갖는 리스트가 생성
+        val randomNumberList = mutableListOf<Int>()
+            .apply {
+                for (i in 1..45) {
+                    if(pickedNumberSet.contains(i)) {
+                        continue
+                    }
+                    this.add(i)
+                }
+            }
+        randomNumberList.shuffle()
+
+        val pickedNumberList: List<Int> = pickedNumberSet.toList()
+        val restNumberList: List<Int> = randomNumberList.subList(0, 6 - pickedNumberSet.size)
+        val sixNumberList = pickedNumberList + restNumberList
+        return sixNumberList.sorted() //오름차순 정렬
+    }
+
+    private fun initClearButton() {
         binding.clearButton.setOnClickListener {
-            checker = false
+            checkerPressedCNB = false
             pickedNumberSet.clear()
             listOfChosenNumberTextView.forEach {
                 it.isVisible = false
             }
         }
-        binding.chooseNumberButton.setOnClickListener {
+    }
+
+    private fun initChoiceNumberButton() {
+        binding.choiceNumberButton.setOnClickListener {
             //[START 예외처리]
-            if(checker){ //이미 createButton 버튼을 눌러 6개 번호를 얻었다면 토스트 메세지를 띄움
-                Toast.makeText(this, "초기화 후 시도해주세요.", Toast.LENGTH_SHORT).show()
+            if(checkerPressedCNB){ //이미 createButton 버튼을 눌러 6개 번호를 얻었다면 토스트 메세지를 띄움
+                Toast.makeText(this, R.string.toast_initialize, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             if (pickedNumberSet.size >= 5) {
-                Toast.makeText(this, "지정 번호는 최대 5개까지 가능합니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.toast_max, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             if (pickedNumberSet.contains(binding.numberPicker.value)){
-                Toast.makeText(this, "이미 지정된 번호입니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.toast_overlap, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             //[END 예외처리]
@@ -74,32 +116,6 @@ class MainActivity : AppCompatActivity() {
             pickedNumberSet.add(binding.numberPicker.value)
             //[END NumberPicker 값 UI 세팅]
         }
-    }
-
-    private fun makeRandomNumber(): List<Int> {
-        //[START 방법1. set 을 이용해 랜덤 숫자 생성]
-//        val numberSet = mutableSetOf<Int>()
-//        while(numberSet.size < 6) {
-//            val rangeNumber = (1..45).random()
-//            numberSet.add(rangeNumber)
-//        }
-//        return numberSet
-        //[END 방법1. set 을 이용해 랜덤 숫자 생성]
-
-        //[START 방법2. list 를 이용해 램덤 숫자 생성]
-        val _numberList = mutableListOf<Int>()
-            .apply {
-                for (i in 1..45) {
-                    if(pickedNumberSet.contains(i)) {
-                        continue
-                    }
-                    this.add(i)
-                }
-            }
-        _numberList.shuffle()
-        val numberList = pickedNumberSet.toList() + _numberList.subList(0, 6 - pickedNumberSet.size)
-        return numberList.sorted()
-        //[END 방법2. list 를 이용해 램덤 숫자 생성]
     }
 
     private fun setCircleBackground(number:Int, textView: TextView){
