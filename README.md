c223f1f2bd18490bc2142a091b92802457abcf25
박현준(Hyunjune Park) <parkhj@konai.com>
2022-02-11 17:32:18 +0900

    abstract MainActivity

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
 # SecretDiary
-비밀번호 설정이 가능한 메모장 앱
 
-![cover](https://user-images.githubusercontent.com/89306567/148688482-2e12c484-e1e7-422d-bb7b-48e018d8f8ec.png)
-
-</br>
-1. 사용자 폰트 사용</br>
-res 우클릭  -> New -> Android Rescouce Directory -> font 디렉토리 생성</br>
-res는 대문자를 사용하지 않으므로 소문자로만 파일이름 설정</br>
-xml 파일 android:fontFamily="@font/폰트 이름"</br>
-</br>
-2. AlertDialog
-</br></br>
-3.Thread
-</br></br>
 4. SharedPreference</br>
 앱 설정 값 등 내부 저장소에 간단한 데이터를 저장할 수 있음</br>
 데이터를 Key:Value 로 저장하며 종료되어도 데이터가 남아있음</br>
@@ -23,6 +10,71 @@ SharedPreferences 생성</br>
 데이터 꺼내기</br>
 - sharedPreferences.getString("name", "defaultData")</br>
 </br>
-**참고링크** </br>
-액션바 뒤로가기 버튼 참고 블로그</br>
-https://programmingworld1.tistory.com/15
+
+
+
+
+<img src="https://github.com/HYUNJUNEPARK/ImageRepository/blob/master/3_SecretDiary.png" height="400"/>
+
+---
+1. <a href = "#content1">Font custom</a></br>
+2. <a href = "#content2">AlertDialog</a></br>
+3. <a href = "#content3">Thread</a></br>
+4. <a href = "#content4">SharedPreference</a></br>
+* <a href = "#ref">참고링크</a>
+---
+><a id = "content1">**Font custom**</a></br>
+
+
+
+res 우클릭  -> New -> Android Rescouce Directory -> font 디렉토리 생성(res 는 대문자를 사용하지 않으므로 소문자로만 파일이름 설정)
+
+'android:fontFamily="@font/폰트 이름" //xml'
+
+
+<br></br>
+<br></br>
+
+><a id = "content2">**AlertDialog**</a></br>
+```kotlin
+//코드 샘플
+```
+<br></br>
+<br></br>
+
+><a id = "content3">**Thread**</a></br>
+```kotlin
+//코드샘플
+```
+<br></br>
+<br></br>
+
+><a id = "content4">**SharedPreference**</a></br>
+
+
+
+```kotlin
+//1. onCreate 영역 내부에서 SharedPreferences 생성
+val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+//2. 쓰기
+
+
+//3. 읽기
+
+
+
+```
+<br></br>
+<br></br>
+
+---
+><a id = "ref">**참고링크**</a></br>
+
+
+액션바 뒤로가기 버튼</br>
+https://programmingworld1.tistory.com/15</br>
+
+SharedPreference 설명</br>
+http://zeany.net/24</br>
+
--- app/src/main/java/com/june/secretdiary/MainActivity.kt
+++ app/src/main/java/com/june/secretdiary/MainActivity.kt
@@ -2,11 +2,11 @@ package com.june.secretdiary
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.edit
 import com.june.secretdiary.databinding.ActivityMainBinding
@@ -19,46 +19,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //액션바 세팅
-        setActionBar()
+        val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE) //SharedPreferences 생성
 
-        //[START Setting numberPicker]
-        binding.numberPicker1.apply {
-            minValue = 0
-            maxValue = 9
-        }
-        binding.numberPicker2.apply {
-            minValue = 0
-            maxValue = 9
-        }
-        binding.numberPicker3.apply {
-            minValue = 0
-            maxValue = 9
-        }
-        //[END Setting numberPicker]
+        setHomeBtnActionBar()
+        initNumberPickers()
+        initOpenButton(passwordPreferences)
+        initChangePWButton(passwordPreferences)
+    }
 
-        //SharedPreferences 생성
-        val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
 
+    private fun initOpenButton(passwordPreferences : SharedPreferences) {
         binding.openButton.setOnClickListener {
-            //[START 예외처리]
             if (changePasswordMode) {
-                Toast.makeText(this, "비밀번호 변경 중입니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.changing_pw, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            //[END 예외처리]
+            val userInputPW = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
 
-            //[START 비밀번호 확인 -> 페이지 이동 or AlertDialog]
-            val passwordFromUser = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
             /* SharedPreferences 에 저장된 값 꺼내오기
             SharedPreferences 객체.get데이터타입("SharedPreferences 이름", "default value") */
-            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+            if (passwordPreferences.getString("password", "000").equals(userInputPW)) {
                 startActivity(Intent(this, DiaryActivity::class.java))
             } else {
                 showAlertDialog()
             }
-            //[END 비밀번호 확인 -> 페이지 이동 or AlertDialog]
+
         }
+    }
+
+    private fun initChangePWButton(passwordPreferences : SharedPreferences) {
         binding.changePasswordButton.setOnClickListener {
             val passwordFromUser = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
             //changePasswordMode 활성화(true) -> 비밀번호 변경 가능 -> changePasswordMode 비활성화(false)
@@ -73,12 +62,12 @@ class MainActivity : AppCompatActivity() {
                 changePasswordMode = false
                 binding.changePasswordButton.setBackgroundColor(Color.BLACK)
                 initializeNumberPicker()
-                Toast.makeText(this, "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show()
-            //changePasswordMode 비활성화(false) -> changePasswordMode 활성화(true)
+                Toast.makeText(this, R.string.changed_pw, Toast.LENGTH_SHORT).show()
+                //changePasswordMode 비활성화(false) -> changePasswordMode 활성화(true)
             } else {
                 if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
                     changePasswordMode = true
-                    Toast.makeText(this, "변경할 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, R.string.insert_pw, Toast.LENGTH_SHORT).show()
                     binding.changePasswordButton.setBackgroundColor(Color.RED)
                 } else {
                     showAlertDialog()
@@ -86,22 +75,41 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private fun showAlertDialog() {
-        AlertDialog.Builder(this)
-            .setTitle("Error")
-            .setMessage("비밀번호가 잘못되었습니다.")
-            .setPositiveButton("확인") { _, _ ->
-                initializeNumberPicker()
-            }
-            .create()
-            .show()
+
+    private fun initNumberPickers() {
+        binding.numberPicker1.apply {
+            minValue = 0
+            maxValue = 9
+        }
+        binding.numberPicker2.apply {
+            minValue = 0
+            maxValue = 9
+        }
+        binding.numberPicker3.apply {
+            minValue = 0
+            maxValue = 9
+        }
     }
+
     private fun initializeNumberPicker(){
         binding.numberPicker1.value = 0
         binding.numberPicker2.value = 0
         binding.numberPicker3.value = 0
     }
-    private fun setActionBar(){
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.error)
+            .setMessage(R.string.wrong_pw)
+            .setPositiveButton(R.string.confirm) { _, _ ->
+                initializeNumberPicker()
+            }
+            .create()
+            .show()
+    }
+
+
+    private fun setHomeBtnActionBar(){
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
     }
