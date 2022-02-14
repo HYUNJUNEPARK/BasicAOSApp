7c89ca4b1c443936bc5fd7d1d9fe3266cf36240c
박현준(Hyunjune Park) <parkhj@konai.com>
2022-02-14 13:55:53 +0900

    refactoring

--- app/src/main/java/com/june/secretdiary/DiaryActivity.kt
+++ app/src/main/java/com/june/secretdiary/DiaryActivity.kt
@@ -11,34 +11,41 @@ import com.june.secretdiary.databinding.ActivityDiaryBinding
 
 class DiaryActivity : AppCompatActivity() {
     private val binding by lazy { ActivityDiaryBinding.inflate(layoutInflater) }
-    private val handler = Handler(Looper.getMainLooper()) //메인 스레드에 연결된 핸들러
+    private val handler = Handler(Looper.getMainLooper()) //메인 스레드에 연결된 핸들러로 Runnable 객체를 처리
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //SharedPreferences 생성
         createPreferencesAndUI()
+        initInitializeButton()
+        createRunnable()
+    }
 
+    private fun createPreferencesAndUI() {
+        val diaryPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+        binding.diaryEditText.setText(diaryPreferences.getString("diary", ""))
+    }
+
+    private fun createRunnable() {
         val saveRunnable = Runnable {
-            //비동기적 저장
             getSharedPreferences("diary", Context.MODE_PRIVATE).edit(false) {
-                putString("detail", binding.diaryEditText.text.toString())
+                putString("diary", binding.diaryEditText.text.toString())
             }
         }
+        autoSave(saveRunnable)
+    }
 
-        //텍스트 수정을 잠시 멈추면(500ms) 자동 저장
+    private fun autoSave(saveRunnable : Runnable) {
         binding.diaryEditText.addTextChangedListener {
             handler.removeCallbacks(saveRunnable) //postDelayed() 가 실행되기 전에 runnable 이 있다면 지움
             handler.postDelayed(saveRunnable, 500) //500ms 에 한번씩 runnable 호출
         }
+    }
+
+    private fun initInitializeButton() {
         binding.initializeButton.setOnClickListener {
             binding.diaryEditText.setText("")
         }
-    }
-    private fun createPreferencesAndUI() {
-        //SharedPreferences 생성 & Preference 저장된 값 가져오기
-        val diaryPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
-        binding.diaryEditText.setText(diaryPreferences.getString("detail", ""))
     }
 }
--- app/src/main/java/com/june/secretdiary/MainActivity.kt
+++ app/src/main/java/com/june/secretdiary/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.june.secretdiary
 
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
@@ -13,20 +12,25 @@ import com.june.secretdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private var changePasswordMode = false //비밀번호를 수정하는 동안 다른 작업을 할 수 없게 하는 변수
+    private var changePasswordMode = false //true 일 때만 PW 변경 가능
+    private var passWord = "000"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE) //SharedPreferences 생성
+        val passwordPreferences: SharedPreferences = getSharedPreferences("password", MODE_PRIVATE) //SharedPreferences 생성
 
-        setHomeBtnActionBar()
-        initNumberPickers()
+        initPassWord(passwordPreferences)
         initOpenButton(passwordPreferences)
         initChangePWButton(passwordPreferences)
+        setHomeBtnActionBar()
+        initNumberPickers()
     }
 
+    private fun initPassWord(passwordPreferences : SharedPreferences) {
+        passwordPreferences.edit(true) { putString("password", passWord) }
+    }
 
     private fun initOpenButton(passwordPreferences : SharedPreferences) {
         binding.openButton.setOnClickListener {
@@ -36,36 +40,25 @@ class MainActivity : AppCompatActivity() {
             }
             val userInputPW = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
 
-            /* SharedPreferences 에 저장된 값 꺼내오기
-            SharedPreferences 객체.get데이터타입("SharedPreferences 이름", "default value") */
-            if (passwordPreferences.getString("password", "000").equals(userInputPW)) {
+            if (passwordPreferences.getString("password", "000") == userInputPW) {
                 startActivity(Intent(this, DiaryActivity::class.java))
             } else {
                 showAlertDialog()
             }
-
         }
     }
 
     private fun initChangePWButton(passwordPreferences : SharedPreferences) {
         binding.changePasswordButton.setOnClickListener {
-            val passwordFromUser = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
-            //changePasswordMode 활성화(true) -> 비밀번호 변경 가능 -> changePasswordMode 비활성화(false)
+            passWord = "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
             if (changePasswordMode) {
-                /*SharedPreferences 값 수정
-                - edit 함수에 람다로 SharedPreferences 값 수정 가능
-                - commit() : 동기적 저장 (파일이 다 저장될때까지 UI를 멈추고 기다림) - 가벼운 작업에서 사용
-                - apply() : 비동기적 저장 */
-                passwordPreferences.edit(true) {
-                    putString("password", passwordFromUser)
-                }
+                passwordPreferences.edit(true) { putString("password", passWord) }
                 changePasswordMode = false
                 binding.changePasswordButton.setBackgroundColor(Color.BLACK)
                 initializeNumberPicker()
                 Toast.makeText(this, R.string.changed_pw, Toast.LENGTH_SHORT).show()
-                //changePasswordMode 비활성화(false) -> changePasswordMode 활성화(true)
             } else {
-                if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                if (passwordPreferences.getString("password", "000") == passWord) {
                     changePasswordMode = true
                     Toast.makeText(this, R.string.insert_pw, Toast.LENGTH_SHORT).show()
                     binding.changePasswordButton.setBackgroundColor(Color.RED)
@@ -107,7 +100,6 @@ class MainActivity : AppCompatActivity() {
             .create()
             .show()
     }
-
 
     private fun setHomeBtnActionBar(){
         val actionBar = supportActionBar
