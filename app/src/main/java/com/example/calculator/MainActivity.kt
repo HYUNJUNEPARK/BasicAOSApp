99e5fa6e58221e2d23e2f82e5fd78fc82dad830a
박현준(Hyunjune Park) <parkhj@konai.com>
2022-03-11 16:32:46 +0900

    UI

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+    }
+
+    fun buttonClicked(view: View) {
+
+    }
+
+    fun clearButtonClicked(view: View) {
+
+    }
+    fun historyButtonClicked(view: View) {
+
+    }
+    fun resultButtonClicked(view: View) {
+
     }
 }
