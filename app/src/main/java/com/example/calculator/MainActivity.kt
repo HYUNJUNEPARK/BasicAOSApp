056bd8eb0ee1acbeb120efa017d0d1ca57407bc8
박현준(Hyunjune Park) <parkhj@konai.com>
2022-03-15 18:27:53 +0900

    numberButtonClicked

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,29 +3,75 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var isOperator = false
+    private var hasOperator = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-
     }
 
     fun buttonClicked(view: View) {
+        when(view.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonModulo -> operatorButtonClicked("%")
+            R.id.buttonMulti -> operatorButtonClicked("*")
+            R.id.buttonDivider -> operatorButtonClicked("/")
+        }
+    }
+
+    private fun numberButtonClicked(number: String) {
+        if (isOperator) {
+            binding.expressionTextView.append(" ")
+        }
+        isOperator = false
+
+        val expressionText = binding.expressionTextView.text.split(" ")
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15) {
+            Toast.makeText(this, "15자리 까지만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        else if(expressionText.last().isEmpty() && number == "0") {
+            Toast.makeText(this, "0은 제일 앞에 올 수 없습니다.", Toast.LENGTH_SHORT).show()
+        }
+        binding.expressionTextView.append(number)
+
+        //TODO resultTextView 실시간으로 계산 결과를 넣어야하는 기능
+
+    }
+
+    private fun operatorButtonClicked(operator: String) {
 
     }
 
     fun clearButtonClicked(view: View) {
 
     }
-    fun historyButtonClicked(view: View) {
+
+
+
+    fun resultButtonClicked(view: View) {
 
     }
-    fun resultButtonClicked(view: View) {
+
+    fun historyButtonClicked(view: View) {
 
     }
 }
